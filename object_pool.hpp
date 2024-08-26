c9cb0774f1272cfcb36560182c5b97c77ed30b8a
AChar <xyconly@163.com>
2024-08-26 11:43:50 +0800

    Update object_pool.hpp

--- object_pool.hpp
+++ object_pool.hpp
@@ -3,25 +3,75 @@ File name:  object_pool.hpp
 Author:     AChar
 Version:
 Date:
-Purpose: Ìá¹©×Ô¶¯ÊÍ·ÅµÄ¶ÔÏó³Ø
-Note:    ±ØĞëÈ·±£Ê¹ÓÃ¶ÔÏóµÄÉúÃüÖÜÆÚµÍÓÚ¶ÔÏó³Ø±¾Éí
-         È·±£ËùÓĞ×ÓÏî¾ùÊÍ·Åºó·½¿ÉÎö¹¹¶ÔÏó³Ø
+Purpose: æä¾›è‡ªåŠ¨é‡Šæ”¾çš„å¯¹è±¡æ± 
+Note:    å¿…é¡»ç¡®ä¿ä½¿ç”¨å¯¹è±¡çš„ç”Ÿå‘½å‘¨æœŸä½äºå¯¹è±¡æ± æœ¬èº«
+         ç¡®ä¿æ‰€æœ‰å­é¡¹å‡é‡Šæ”¾åæ–¹å¯ææ„å¯¹è±¡æ± 
 *************************************************/
 #pragma once
 #include <thread>
 #include <mutex>
 #include <memory>
 #include <queue>
+#include <stack>
 #include <functional>
 
 namespace BTool {
-
     template <typename T>
     class ObjectPool {
+    public:
+        using PtrType = std::shared_ptr<T>;
+        ObjectPool(int capacity = 0) : m_size(capacity) {
+            if (capacity > 0) {
+                for(int i = 0; i < capacity; ++i) {
+                    m_pool.push(new T);
+                }
+            }
+        }
+
+        // è·å–ä¸€ä¸ª T ç±»å‹çš„å¯¹è±¡
+        template<typename ...Args>
+        PtrType acquire(Args... args) {
+            std::lock_guard<std::mutex> lock(m_mtx);
+            if (m_pool.empty()) {
+                ++m_size;
+                if constexpr (std::is_trivial<T>::value) {
+                    return PtrType(new T{ std::forward<Args>(args)... }, [this](T* ptr) { release(ptr); });
+                }
+                else {
+                    return PtrType(new T( std::forward<Args>(args)... ), [this](T* ptr) { release(ptr); });
+                }
+            }
+            else {
+                auto pre_ptr = m_pool.top();
+                PtrType ptr = nullptr;
+                if constexpr (std::is_trivial<T>::value) {
+                    ptr = PtrType(new (pre_ptr) T{ std::forward<Args>(args)... }, [this](T* ptr) { release(ptr); });
+                }
+                else {
+                    ptr = PtrType(new (pre_ptr) T( std::forward<Args>(args)... ), [this](T* ptr) { release(ptr); });
+                }
+                m_pool.pop();
+                return ptr;
+            }
+        }
+
+        void release(T* ptr) {
+            std::lock_guard<std::mutex> lock(m_mtx);
+            m_pool.push(ptr);
+        }
+
+    private:
+        size_t                          m_size;
+        std::mutex                      m_mtx;
+        std::stack<T*>                  m_pool;
+    };
+
+    template <typename T>
+    class ObjectPoolUni {
         using deleter_function = std::function<void(T*)>;
     public:
         using Type = std::unique_ptr<T, deleter_function>;
-        ObjectPool(size_t capacity = 0) : m_capacity(capacity), m_size(0){}
+        ObjectPoolUni(){}
 
         template<typename ...Args>
         Type acquire(Args... args) {
@@ -36,42 +86,32 @@ namespace BTool {
             };
 
             std::lock_guard<std::mutex> lock(m_mtx);
-            for (;;) {
-                // ²»Îª¿ÕÔòÀûÓÃÔ­Ê¼Êı¾İ
-                if (!m_pool.empty()) {
-                    auto ptr = std::move(m_pool.front());
-                    m_pool.pop();
-                    auto real_ptr = ptr.release();
-                    if constexpr (std::is_trivial<T>::value) {
-                        real_ptr = new (real_ptr) T{ std::forward<Args>(args)... };
-                    }
-                    else {
-                        real_ptr = new (real_ptr) T(std::forward<Args>(args)...);
-                    }
-
-                    return Type(real_ptr, deleter);
-                }
-                // Îª¿ÕÔòÅĞ¶ÏÊÇ·ñÒÑÂú, ÒÑÂúÔòÇ¿ÖÆµÈ´ı
-                if(m_capacity > 0 && m_size >= m_capacity) {
-                    std::this_thread::yield();
-                    continue;
-                }
-                // Î´ÂúÔòĞÂ·ÖÅä
-                ++m_size;
-                auto obj = std::malloc(sizeof(T));
-                // printf("malloc:%p \n", (const char*)obj);
+            // ä¸ä¸ºç©ºåˆ™åˆ©ç”¨åŸå§‹æ•°æ®
+            if (!m_pool.empty()) {
+                auto ptr = std::move(m_pool.front());
+                m_pool.pop();
+                auto real_ptr = ptr.release();
                 if constexpr (std::is_trivial<T>::value) {
-                    return Type(new (obj) T{ std::forward<Args>(args)... }, deleter);
+                    real_ptr = new (real_ptr) T{ std::forward<Args>(args)... };
                 }
                 else {
-                    return Type(new (obj) T(std::forward<Args>(args)...), deleter);
+                    real_ptr = new (real_ptr) T(std::forward<Args>(args)...);
                 }
+
+                return Type(real_ptr, deleter);
+            }
+            // æœªæ»¡åˆ™æ–°åˆ†é…
+            auto obj = std::malloc(sizeof(T));
+            // printf("malloc:%p \n", (const char*)obj);
+            if constexpr (std::is_trivial<T>::value) {
+                return Type(new (obj) T{ std::forward<Args>(args)... }, deleter);
+            }
+            else {
+                return Type(new (obj) T(std::forward<Args>(args)...), deleter);
             }
         }
 
     private:
-        size_t                                              m_capacity;
-        size_t                                              m_size;
         std::mutex                                          m_mtx;
         std::queue<std::unique_ptr<T, deleter_function>>    m_pool;
     };
